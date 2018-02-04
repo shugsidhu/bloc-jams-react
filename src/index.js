8484e21208e75cdacc43404c7c570d3a4469fb8d
Shug Sidhu <Shug@priyas-mbp.attlocal.net>
2018-02-04 13:36:54 -0800

    checkpoint landing

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(
   <BrowserRouter>
     <App />
-  </BroweserRouter>
+  </BrowserRouter>
   , document.getElementById('root'));
 registerServiceWorker();
